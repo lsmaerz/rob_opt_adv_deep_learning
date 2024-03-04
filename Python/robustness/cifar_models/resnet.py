ab91270a065fd83d60233da2423e76641009ac84
Lars März <139419740+lsmaerz@users.noreply.github.com>
2024-03-05 00:44:20 +0100

    Add files via upload

---
+++ Python/robustness/cifar_models/resnet.py
@@ -0,0 +1,409 @@
+'''ResNet in PyTorch.
+For Pre-activation ResNet, see 'preact_resnet.py'.
+Reference:
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from ..tools.custom_modules import SequentialWithArgs, FakeReLU
+import math
+
+###################################################################### Copied from LocusLab's github (https://github.com/locuslab/fast_adversarial.git):
+
+# construct PreAct Block according to Locus' paper
+class PreActBlock(nn.Module):
+    '''Pre-activation version of the BasicBlock.'''
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(PreActBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+            )
+
+    def forward(self, x, fake_relu=False):
+        out = F.relu(self.bn1(x))
+        shortcut = self.shortcut(x) if hasattr(self, 'shortcut') else x
+        out = self.conv1(out)
+        out = self.conv2(F.relu(self.bn2(out)))
+        out += shortcut
+        return out
+
+# construct bottleneck according to Locus' paper
+class PreActBottleneck(nn.Module):
+    '''Pre-activation version of the original Bottleneck module.'''
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(PreActBottleneck, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+            )
+
+    def forward(self, x, fake_relu=False):
+        out = F.relu(self.bn1(x))
+        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
+        out = self.conv1(out)
+        out = self.conv2(F.relu(self.bn2(out)))
+        out = self.conv3(F.relu(self.bn3(out)))
+        out += shortcut
+        return out
+
+# construct PreAct ResNet18 model according to Locus' paper
+class PreActResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(PreActResNet, self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.bn = nn.BatchNorm2d(512 * block.expansion)
+        self.linear = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return SequentialWithArgs(*layers)
+
+    def forward(self, x, with_latent=False, fake_relu=False, no_relu=False):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out, fake_relu=fake_relu)
+        out = F.relu(self.bn(out))
+        out = F.avg_pool2d(out, 4)
+        pre_out = out.view(out.size(0), -1)
+        final = self.linear(pre_out)
+        if with_latent:
+            return final, pre_out
+        else:
+            return final
+
+################################################### Copied rom Hadisalman's github (https://github.com/Hadisalman/smoothing-adversarial.git):
+
+def conv3x3_hadi(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+# construct basic block according to Hadisalman's paper
+class BasicBlock_hadi(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock_hadi, self).__init__()
+        self.conv1 = conv3x3_hadi(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3_hadi(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x, fake_relu=False):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        if fake_relu:
+            out = FakeReLU.apply(out)
+        else:
+            out = self.relu(out)
+
+        return out
+
+# construct Bottleneck according to Hadisalman's paper
+class Bottleneck_hadi(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck_hadi, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x, fake_relu=False):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        if fake_relu:
+            out = FakeReLU.apply(out)
+        else:
+            out = self.relu(out)
+
+        return out
+
+# construct ResNet110 model according to Hadisalman's paper
+class ResNet_hadi(nn.Module):
+
+    def __init__(self, depth, num_classes=1000, block_name='BasicBlock'):
+        super(ResNet_hadi, self).__init__()
+        # Model type specifies number of layers for CIFAR-10 model
+        if block_name.lower() == 'basicblock':
+            assert (depth - 2) % 6 == 0, 'When use basicblock, depth should be 6n+2, e.g. 20, 32, 44, 56, 110, 1202'
+            n = (depth - 2) // 6
+            block = BasicBlock_hadi
+        elif block_name.lower() == 'bottleneck':
+            assert (depth - 2) % 9 == 0, 'When use bottleneck, depth should be 9n+2, e.g. 20, 29, 47, 56, 110, 1199'
+            n = (depth - 2) // 9
+            block = Bottleneck_hadi
+        else:
+            raise ValueError('block_name shoule be Basicblock or Bottleneck')
+
+
+        self.inplanes = 16
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 16, n)
+        self.layer2 = self._make_layer(block, 32, n, stride=2)
+        self.layer3 = self._make_layer(block, 64, n, stride=2)
+        self.avgpool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return SequentialWithArgs(*layers)
+
+    def forward(self, x, with_latent=False, fake_relu=False, no_relu=False):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)    # 32x32
+
+        x = self.layer1(x)  # 32x32
+        x = self.layer2(x)  # 16x16
+        x = self.layer3(x, fake_relu=fake_relu)  # 8x8
+
+        x = self.avgpool(x)
+        pre_out = x.view(x.size(0), -1)
+        final = self.fc(pre_out)
+        if with_latent:
+            return final, pre_out
+
+        return final
+
+
+def resnet_hadi(**kwargs):
+    """
+    Constructs a ResNet model.
+    """
+    return ResNet_hadi(**kwargs)
+
+########################################################################
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1,
+                          stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes))
+
+    def forward(self, x, fake_relu=False):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        if fake_relu:
+            return FakeReLU.apply(out)
+        return F.relu(out)
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x, fake_relu=False):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        if fake_relu:
+            return FakeReLU.apply(out)
+        return F.relu(out)
+
+
+class ResNet(nn.Module):
+    # feat_scale lets us deal with CelebA, other non-32x32 datasets
+    def __init__(self, block, num_blocks, num_classes=10, feat_scale=1, wm=1):
+        super(ResNet, self).__init__()
+
+        widths = [64, 128, 256, 512]
+        widths = [int(w * wm) for w in widths]
+
+        self.in_planes = widths[0]
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.layer1 = self._make_layer(block, widths[0], num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, widths[1], num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, widths[2], num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, widths[3], num_blocks[3], stride=2)
+        self.linear = nn.Linear(feat_scale*widths[3]*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return SequentialWithArgs(*layers)
+
+    def forward(self, x, with_latent=False, fake_relu=False, no_relu=False):
+        assert (not no_relu),  \
+            "no_relu not yet supported for this architecture"
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out, fake_relu=fake_relu)
+        out = F.avg_pool2d(out, 4)
+        pre_out = out.view(out.size(0), -1)
+        final = self.linear(pre_out)
+        if with_latent:
+            return final, pre_out
+        return final
+
+def ResNet18(**kwargs):
+    return ResNet(BasicBlock, [2,2,2,2], **kwargs)
+
+def ResNet18Wide(**kwargs):
+    return ResNet(BasicBlock, [2,2,2,2], wm=5, **kwargs)
+
+def ResNet18Thin(**kwargs):
+    return ResNet(BasicBlock, [2,2,2,2], wd=.75, **kwargs)
+
+def PreActResNet18(**kwargs):
+    return PreActResNet(PreActBlock, [2,2,2,2], **kwargs)
+
+def ResNet34(**kwargs):
+    return ResNet(BasicBlock, [3,4,6,3], **kwargs)
+
+def ResNet50(**kwargs):
+    return ResNet(Bottleneck, [3,4,6,3], **kwargs)
+
+def ResNet101(**kwargs):
+    return ResNet(Bottleneck, [3,4,23,3], **kwargs)
+
+def ResNet110(**kwargs):
+    return ResNet_hadi(depth=110, **kwargs)
+
+def ResNet152(**kwargs):
+    return ResNet(Bottleneck, [3,8,36,3], **kwargs)
+
+resnet50 = ResNet50
+resnet18 = ResNet18
+resnet34 = ResNet34
+resnet101 = ResNet101
+resnet110 = ResNet110
+resnet152 = ResNet152
+resnet18wide = ResNet18Wide
+preactresnet18 = PreActResNet18
+
+# resnet18thin = ResNet18Thin
+def test():
+    net = ResNet18()
+    y = net(torch.randn(1,3,32,32))
+    print(y.size())
+
